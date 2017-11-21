69d97ce6c16b1c7d9b72be87b1a4233cb6e69914
calebharnell <calebharnell@hotmail.com>
2017-11-21 14:39:05 +1000

    Express Todos

--- express.js
+++ express.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 
 // Our array of students
 let students = ['Caleb', 'Dave', "Fred"];
+// Our array of todos
+let todos = [{task: "Do this", done: false}, {task: "Do that", done: true}]
 
 // BodyParser to make it easy to read POST data
 app.use(bodyParser.json())
@@ -21,6 +23,25 @@ app.post('/students', function(req, res) {
   students.push(req.body.name)
   // Send user a list of students
   res.send(students);
+})
+
+// GET /todos
+app.get('/todos', function(req, res) {
+  // Send user a list of todos
+  res.send(todos);
+})
+
+// POST /todos
+app.post('/todos', function(req, res) {
+  // Add new todo to array
+  todos.push(req.body)
+  // Send user a list of todos
+  res.send(todos);
+})
+
+// All other urls - 404
+app.use(function (req, res, next) {
+  res.status(404).send("Sorry can't find that!")
 })
 
 // Listen on our specified port
